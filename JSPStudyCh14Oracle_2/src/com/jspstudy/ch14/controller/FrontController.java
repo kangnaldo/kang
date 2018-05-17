8dd013555fff08ede7c623cd7a51136c34f0576c
201-24 <201-24@201-24-PC>
2018-05-17 17:46:27 +0900

    ㅇㅋ

--- JSPStudyCh14Oracle_2/src/com/jspstudy/ch14/controller/FrontController.java
+++ JSPStudyCh14Oracle_2/src/com/jspstudy/ch14/controller/FrontController.java
@@ -14,104 +14,123 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.jspstudy.ch14.dao.User;
 import com.jspstudy.ch14.model.CommandProcess;
 import com.jspstudy.ch14.model.ForwardService;
 
 public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
-	
-	
+
 	Map<String, CommandProcess> commandMap = new HashMap<String, CommandProcess>();
-	
-    public FrontController() { 
-        super();
-    }
+
+	public FrontController() {
+		super();
+	}
 
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
 		String commandURI = getInitParameter("commandURI");
-		
+
 		Properties prop = new Properties();
-		
+
 		FileInputStream fis = null;
 		BufferedInputStream bis = null;
 		try {
-								
+
 			String realPath = getServletContext().getRealPath(commandURI);
 			fis = new FileInputStream(realPath);
 			bis = new BufferedInputStream(fis);
-			
+
 			prop.load(bis);
-		}catch(IOException e){
-		}finally{
-			try{
-				if(bis != null) bis.close();
-				if(fis != null) fis.close();
-			}catch(IOException e){
-				
+		} catch (IOException e) {
+		} finally {
+			try {
+				if (bis != null)
+					bis.close();
+				if (fis != null)
+					fis.close();
+			} catch (IOException e) {
+
 			}
 		}
-		
+
 		Iterator iter = prop.keySet().iterator();
-		while(iter.hasNext()){
-			String command = (String)iter.next();
+		while (iter.hasNext()) {
+			String command = (String) iter.next();
 			String className = prop.getProperty(command);
-			
+
 			try {
 				Class<?> commandClass = Class.forName(className);
-				CommandProcess service = (CommandProcess)commandClass.newInstance();
-				
+				CommandProcess service = (CommandProcess) commandClass.newInstance();
+
 				commandMap.put(command, service);
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 				throw new ServletException("FrontController - ClassNotFound");
-				
-			} catch(InstantiationException e){
+
+			} catch (InstantiationException e) {
 				e.printStackTrace();
 				throw new ServletException("FrontController - Instantiation");
-			} catch(IllegalAccessException e){
+			} catch (IllegalAccessException e) {
 				e.printStackTrace();
 				throw new ServletException("FrontController - IllegalAccess");
 			}
 		}
 	}
-	
-	
-	
-	public void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
+
+	public void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String requestURI = request.getRequestURI();
 		String contextPath = request.getContextPath();
 		System.out.println(requestURI + "," + contextPath);
-		
+
 		String command = requestURI.substring(contextPath.length());
 		System.out.println("command : " + command);
 		ForwardService forward = null;
 		CommandProcess service = commandMap.get(command);
-		if(service != null){
+
+		
+
+		if (service != null) {
 			forward = service.getRequestService(request, response);
 		} else {
 			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 		}
 		
-		if(forward != null){
-			if(forward.isRedirect()){//리다이렉트 처리
+		
+		HttpSession session = request.getSession();
+
+		User user = (User) session.getAttribute("user");
+
+		if (user != null) {
+			
+		} else {
+			request.setAttribute("error", "먼저 로그인을 하세요");
+			request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+		}
+		
+		
+
+		if (forward != null) {
+			if (forward.isRedirect()) {// 리다이렉트 처리
 				response.sendRedirect(forward.getPath());
-			}
-			else { //포워드 처리
+			} else { // 포워드 처리
 				RequestDispatcher rd = request.getRequestDispatcher(forward.getPath());
 				rd.forward(request, response);
-						
+
 			}
 		}
 	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doProcess(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doProcess(request, response);
 	}
