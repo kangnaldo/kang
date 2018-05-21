9b311c3352d545c4c80392bb013ebca339d156d1
201-24 <201-24@201-24-PC>
2018-05-21 12:04:20 +0900

    호잇

---
+++ KangTodoProject/src/main/java/com/kang/bean/User.java
@@ -0,0 +1,46 @@
+package com.kang.bean;
+
+public class User {
+	private int id;
+	private String name;
+	private String password;
+	private String email;
+	private String addr;
+	private int userNo;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public int getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+}
---
+++ KangTodoProject/src/main/java/com/kang/dao/TodoDaoImpl.java
@@ -0,0 +1,5 @@
+package com.kang.dao;
+
+public class TodoDaoImpl {
+
+}
